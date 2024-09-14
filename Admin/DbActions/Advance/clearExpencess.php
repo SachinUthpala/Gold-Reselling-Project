0c4f3cc28f79baa9c304480d4abfe80b2aff9ad6
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-09-14 19:50:21 +0530

    some changes

--- Admin/DbActions/Advance/clearExpencess.php
+++ Admin/DbActions/Advance/clearExpencess.php
@@ -6,6 +6,9 @@ session_start();
 $sql = "TRUNCATE TABLE total_expencess";
 $result = mysqli_query($conn , $sql);
 
+$sqk2 = "TRUNCATE TABLE expencess";
+$result2 = mysqli_query($conn , $sqk2);
+
 header("Location: ../../AdminPanel/ExpencessSummery.php");
 
 
