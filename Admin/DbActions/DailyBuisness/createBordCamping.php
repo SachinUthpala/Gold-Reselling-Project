b77aa282c5030e8a34966f7165f929ea550fbb81
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-09-29 17:35:40 +0530

    create bord cost backend part

---
+++ Admin/DbActions/DailyBuisness/createBordCamping.php
@@ -0,0 +1,23 @@
+<?php
+
+
+require_once '../Db.conn.php';
+session_start();
+
+if(isset($_POST['dailyBuisness'])){
+    $costType = $_POST['Weight'];
+    $costAmount = $_POST['BuyingPrice'];
+    $date = $_POST['Date'];
+    $time = $_POST['Time'];
+    $sql = "INSERT INTO `bordcampingcost`( `costType`, `date`, `time`, `amount`) VALUES ( '$costType' , '$date' , '$time' , $costAmount )";
+    $result = mysqli_query($conn , $sql);
+
+    $_SESSION['TaskCreated'] = 1;
+    header("Location: ../../AdminPanel/CreateDailyBoardCamping.php");
+}
+
+
+
+
+
+?>
