ceb0d5639f273e52c0527ba1760f7dd4b2ce1596
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-09-30 00:35:01 +0530

    create daily bord camping cost

--- Admin/AdminPanel/DailyReport.php
+++ Admin/AdminPanel/DailyReport.php
@@ -869,6 +869,49 @@ $allexpencess = $result_expencess->num_rows ;
             </div>
           </div>
 
+          <hr style="width: 3px; background-color: #7CFC00;">
+
+          <h6>3.) Daily Board Camping Costs</h6>
+
+          <?php
+                $sql9 = "SELECT * FROM bordcampingcost WHERE date = CURDATE()";
+                $result9 = mysqli_query($conn , $sql9);
+                $totalDailyBoardCampingCost = 0 ; 
+
+                while($rows9 = $result9-> fetch_assoc()){
+                    $totalDailyBoardCampingCost = $totalDailyBoardCampingCost + (float)$rows9['amount'];
+                } 
+            ?>
+
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="card">
+                <div class="card-statistic-4">
+                  <div class="align-items-center justify-content-between">
+                    <div class="row ">
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                        <div class="card-content">
+                          <h5 class="font-15">Current Day</h5>
+                          <h2 class="mb-3 font-18"><?php 
+                                
+                                    echo '<span style= "color:red;">Rs.'.$totalDailyBoardCampingCost.'.00</span>';
+                               
+                          ?></h2>
+                          <p class="mb-0"><span class="col-orange">
+                          
+                          </span>Total Current Day Board Camping Cost</p>
+                        </div>
+                      </div>
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                        <div class="banner-img">
+                          <img src="assets/img/banner/2.png" alt="">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
 
 
 
